89de6b76d54159f64f55dfc53f01e8bf1eb09b0e
tanphat <tanphat1711999@gmail.com>
2020-07-07 22:59:00 +0700

    update api for signup component

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 const app = express();
 const PORT = 5000;
@@ -21,6 +22,7 @@ mongoose.connection.on("error", (err) => {
   console.log("Connected to mongo fail", err);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(authRoute);
 app.use(postRoute);
