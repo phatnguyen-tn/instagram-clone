f43f8d661cce7d03f3abb1cf16bc5e312efb3e54
tanphat <tanphat1711999@gmail.com>
2020-07-20 20:32:21 +0700

    update follow function

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 const PORT = 5000;
-const {
-  MONGO_URL
-} = require("./configs/key");
+const { MONGO_URL } = require("./configs/key");
 
-const authRoute = require('./routes/auth.route');
-const postRoute = require('./routes/post.route');
+const authRoute = require("./routes/auth.route");
+const postRoute = require("./routes/post.route");
+const userRoute = require("./routes/user.route");
 
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 mongoose.connection.on("connected", () => {
   console.log("Connectd to mongo successfully");
@@ -26,6 +26,7 @@ app.use(cors());
 app.use(express.json());
 app.use(authRoute);
 app.use(postRoute);
+app.use(userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const { ObjectId } = mongoose.Schema.Types;
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -13,6 +13,13 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  pic: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/cnq/image/upload/v1586197723/noimage_d4ipmd.png",
+  },
+  followers: [{ type: ObjectId, ref: "User" }],
+  followings: [{ type: ObjectId, ref: "User" }],
 });
 
 module.exports = mongoose.model("User", userSchema, "users");
